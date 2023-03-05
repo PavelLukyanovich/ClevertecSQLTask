573b79e5be9728683ffafbc7a2bf43ab2709659f
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-03-05 15:52:54 +0300

    feat: add second request

--- resorces/sql/sql-task.sql
+++ resorces/sql/sql-task.sql
@@ -1,7 +1,17 @@
---Вывести к каждому самолету класс обслуживания и количество мест этого класса
+/*--Вывести к каждому самолету класс обслуживания и количество мест этого класса
 SELECT model -> 'ru', aircrafts_data.aircraft_code, fare_conditions, COUNT(seat_no)
 FROM aircrafts_data, seats
 WHERE seats.aircraft_code = aircrafts_data.aircraft_code
 GROUP BY aircrafts_data.aircraft_code, model -> 'ru', fare_conditions
-ORDER BY model
+ORDER BY model*/
+
+--Найти 3 самых вместительных самолета (модель + кол-во мест)
+SELECT model -> 'ru', COUNT(seat_no)
+FROM aircrafts_data, seats
+WHERE aircrafts_data.aircraft_code = seats.aircraft_code
+GROUP BY model
+ORDER BY count(seat_no) desc
+LIMIT 3
+
+
 
