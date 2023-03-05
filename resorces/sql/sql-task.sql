6fd48331b5c491d1f5473f424879321a5cbaca22
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-03-05 19:18:18 +0300

    feat: add 3 request

--- resorces/sql/sql-task.sql
+++ resorces/sql/sql-task.sql
@@ -1,9 +1,9 @@
-/*--Вывести к каждому самолету класс обслуживания и количество мест этого класса
+--Вывести к каждому самолету класс обслуживания и количество мест этого класса
 SELECT model -> 'ru', aircrafts_data.aircraft_code, fare_conditions, COUNT(seat_no)
 FROM aircrafts_data, seats
 WHERE seats.aircraft_code = aircrafts_data.aircraft_code
 GROUP BY aircrafts_data.aircraft_code, model -> 'ru', fare_conditions
-ORDER BY model*/
+ORDER BY model;
 
 --Найти 3 самых вместительных самолета (модель + кол-во мест)
 SELECT model -> 'ru', COUNT(seat_no)
@@ -11,7 +11,14 @@ FROM aircrafts_data, seats
 WHERE aircrafts_data.aircraft_code = seats.aircraft_code
 GROUP BY model
 ORDER BY count(seat_no) desc
-LIMIT 3
+LIMIT 3;
+
+--Вывести код,модель самолета и места не эконом класса для самолета 'Аэробус A321-200' с сортировкой по местам
+SELECT aircrafts_data.aircraft_code, model -> 'ru', seat_no
+FROM aircrafts_data, seats
+WHERE seats.fare_conditions NOT IN ('Economy') AND model -> 'ru' = '"Аэробус A321-200"'
+GROUP BY aircrafts_data.aircraft_code, model -> 'ru', seat_no
+ORDER BY seat_no;
 
 
 
